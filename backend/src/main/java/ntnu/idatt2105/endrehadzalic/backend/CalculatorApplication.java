285e48101e9d6b2a723d0f163893c513ee8f9cb3
Endré Hadzalic <endrehad@stud.ntnu.no>
2021-05-05 13:29:07 +0200

    INIT

---
+++ backend/src/main/java/ntnu/idatt2105/endrehadzalic/backend/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2105.endrehadzalic.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ntnu/idatt2105/endrehadzalic/backend/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2105.endrehadzalic.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
